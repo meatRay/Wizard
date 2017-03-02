ab0aed4b2a9d6de327173bc4c14416ff494b6328
Shannon <Shannon>
2017-03-02 15:52:13 -0600

    typing into  git on a visual  keyboard '

---
+++ Wizard.Scripts/Rune.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wizard.Runes.TinyPG;
+
+namespace Wizard.Runes
+{
+	public class Rune
+	{
+		public string Word { get; private set; }
+
+		public Rune(string word)
+		{
+			Word = word;
+		}
+		
+		public static Rune[] CreateFrom( string input, string file_name )
+		{
+			RuneParser = new Parser(new Scanner());
+			ParseTree tree = new RuneTree();
+			tree = RuneParser.Parse(input, file_name, tree);
+			return tree.Eval(null) as Rune[];
+		}
+
+		private static Parser RuneParser;
+	}
+	public class TokenRune : Rune
+	{
+		public TokenRune(string r_name, object token)
+			: base(r_name)
+		{
+			_token = token;
+		}
+		public T Read<T>()
+			=> (T)_token;
+
+		private object _token;
+	}
+	public class ComplexRune : Rune
+	{
+		public Rune[] SubRunes { get; private set; }
+		public ComplexRune(string word, Rune[] subrunes)
+			: base(word)
+		{
+			SubRunes = subrunes;
+		}
+
+		public Rune Read(string rune_name)
+			=> SubRunes.Where(r => r.Word.Equals(rune_name, StringComparison.OrdinalIgnoreCase))
+				.FirstOrDefault();
+		public IEnumerable<Rune> ReadAll(string rune_name)
+			=> SubRunes.Where(r => r.Word.Equals(rune_name, StringComparison.OrdinalIgnoreCase));
+
+		public R Read<R>(string rune_name) where R : Rune
+			=> Read(rune_name) as R;
+		public IEnumerable<R> ReadAll<R>(string rune_name) where R : Rune
+			=> ReadAll(rune_name).Cast<R>();
+	}
+}
---
+++ Wizard.Scripts/TinyPG/RuneTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wizard.Runes.TinyPG
+{
+	class RuneTree : ParseTree
+	{
+		protected override object EvalStart(ParseTree tree, params object[] paramlist)
+		{
+			return Nodes.Where(n => n.Token.Type == TokenType.Item)
+					.Select(i => i.Eval(tree, paramlist) as Rune)
+					.ToArray();
+		}
+		protected override object EvalItem(ParseTree tree, params object[] paramlist)
+		{
+			string r_name = Nodes.First(n => n.Token.Type == TokenType.WORD).Text;
+			var val = Nodes.First(n => n.Token.Type == TokenType.Value).Nodes.First();
+			if (val.Token.Type == TokenType.Atom)
+			{
+				object token;
+				var atom = val.Nodes.First();
+				if (atom.Token.Type == TokenType.NUMBER)
+					token = double.Parse(atom.Text);
+				else
+					token = atom.Text.Substring(1, atom.Text.Length - 2);
+				return new TokenRune(r_name, token);
+			}
+			else if (val.Token.Type == TokenType.Group)
+			{
+				new ComplexRune(r_name,
+					val.Nodes.Where(n => n.Token.Type == TokenType.Item)
+					.Select(i => i.Eval(tree, paramlist) as Rune)
+					.ToArray());
+			}
+			else
+				return new Rune(r_name);
+			return null;
+		}
+	}
+}
